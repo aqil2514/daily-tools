49efaac37cf488fa095166778b8060d4b85076a1
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-12-03 09:45:54 +0700

    Hide Image Compressor

--- web/src/features/tools/Images/ImageCompressor/index.tsx
+++ web/src/features/tools/Images/ImageCompressor/index.tsx
@@ -1,16 +1,16 @@
-import { SectionHeader } from "@/components/molecules/section-header";
-import { toolsRegistry } from "../../registry";
-import { ImageCompressorComp } from "./components/image-compressor";
-import { ImageCompressorProvider } from "./provider";
+// import { SectionHeader } from "@/components/molecules/section-header";
+// import { toolsRegistry } from "../../registry";
+// import { ImageCompressorComp } from "./components/image-compressor";
+// import { ImageCompressorProvider } from "./provider";
 
-export default function ImageCompressor() {
-  const tool = toolsRegistry["image-compressor"];
-  return (
-    <ImageCompressorProvider>
-      <div>
-        <SectionHeader title={tool.title} description={tool.description} />
-        <ImageCompressorComp />
-      </div>
-    </ImageCompressorProvider>
-  );
-}
+// export default function ImageCompressor() {
+//   const tool = toolsRegistry["image-compressor"];
+//   return (
+//     <ImageCompressorProvider>
+//       <div>
+//         <SectionHeader title={tool.title} description={tool.description} />
+//         <ImageCompressorComp />
+//       </div>
+//     </ImageCompressorProvider>
+//   );
+// }
