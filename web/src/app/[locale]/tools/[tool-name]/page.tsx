6d2073ead49510697ec4daad9e9d6944f3dab76a
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-12-05 11:37:37 +0700

    Title

--- web/src/app/[locale]/tools/[tool-name]/page.tsx
+++ web/src/app/[locale]/tools/[tool-name]/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   return {
-    title: `${tool.title} | Flowtooly`,
+    title: `${tool.title[locale]} | Flowtooly`,
     description: tool.description[locale],
     keywords: tool.keywords[locale],
   };
