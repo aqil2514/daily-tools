366a3f7739b02d6c13aacb1eed1be62259e8d7e4
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-12-11 10:05:57 +0700

    Fix error ts

--- web/src/features/tools/registry/financial-registry/index.ts
+++ web/src/features/tools/registry/financial-registry/index.ts
@@ -1,9 +1,9 @@
 import { FinancialToolName, ToolRegistryItem } from "@/@types/Tools"; // Asumsi path
 import { financialRegistry01 } from "./01-financial-registry";
 
-export const financialRegistry: Record<FinancialToolName, ToolRegistryItem> = {
+export const financialRegistry = {
   ...financialRegistry01,
-};
+} as Record<FinancialToolName, ToolRegistryItem>;
 
 export const financialToolNames = Object.keys(
   financialRegistry
