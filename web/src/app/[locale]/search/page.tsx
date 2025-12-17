fb3bb94b975cb2d0e8318802cfefd4d23a18f26c
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-12-17 09:26:33 +0700

    Search page seo

--- web/src/app/[locale]/search/page.tsx
+++ web/src/app/[locale]/search/page.tsx
@@ -1,13 +1,23 @@
+import { canonicalUrl } from "@/constants/seo";
 import SearchPageTemplate from "@/features/search-page";
 import { searchMetadata } from "@/features/search-page/seo/metadata";
+import { Metadata } from "next";
 import { getLocale } from "next-intl/server";
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
   const locale = (await getLocale()) as "en" | "id";
-  return searchMetadata[locale];
+  return {
+    ...searchMetadata[locale],
+    robots: {
+      follow: true,
+      index: false,
+    },
+    alternates:{
+      canonical: canonicalUrl(locale, "/search")
+    }
+  };
 }
 
-
 export default function SearchPage() {
-  return <SearchPageTemplate />
+  return <SearchPageTemplate />;
 }
