cc4d423e3f3f62de74a9cb3717577e25ffa9df07
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-12-21 10:50:18 +0700

    Sentry integration

---
+++ web/src/instrumentation-client.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+
+  tracesSampleRate: 0,
+  enableLogs: false,
+  sendDefaultPii: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
